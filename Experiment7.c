40f393d4509c168476ef49065dfcf948988e53a4
saba9323 <sabaiqbalansari2006@eng.rizvi.edu.in>
2024-11-24 04:54:40 +0530

    Update Experiment7.c

--- Experiment7.c
+++ Experiment7.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
 
+int main(){
+    int size , i, largest=0;
+    
+    printf("\t\t***Array Largest Number Finder***\n\n");
+    
+    printf("Enter Array Size:");
+    scanf("%d",&size);
+    
+    int numbers[size];
+    
+    printf("Enter Array Size: \n");
+    for(i=0; i<size; i++){
+        printf("%d",i);
+        scanf("%d",&numbers[i]);
+        
+        if(numbers[i]>largest){
+            largest = numbers[i];
+        }
+    }
+    printf("\nlargest Numbers: %d", largest);
+    
+    return 0;
+}
